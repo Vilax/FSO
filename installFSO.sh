06427d962e13db43b032c8ae2e09eb2b87f585b8
Vilax <jlvilasprieto@gmail.com>
2020-05-27 12:35:44 -0400

    Update installFSO.sh

--- installFSO.sh
+++ installFSO.sh
@@ -21,7 +21,7 @@ echo " "
 echo " "
 echo "Creating a virtual enviroment..."
 
-pip install -U python3-venv
+pip install -U virtualenv
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade pip
